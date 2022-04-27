03af51951491d14c10ddeb12e3fc08da98d562e4
DarthEggo <95883373+DarthEggo@users.noreply.github.com>
2022-04-26 17:35:05 -0700

    censored secrets

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ import praw;
 reddit = praw.Reddit(
     user_agent="KiAdiMundiBot by u/DarthEggo1",
     client_id="0XmospxqKil_CVVajzJF6A",
-    client_secret="XNTVkAXJ1tlXEuLa__Dy1vohc0gTDg",
+    client_secret="censored",
     username="Ki-Adi-MundiBot",
-    password="droidattackwookies",
+    password="censored",
 )
 import time
 
